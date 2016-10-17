be5d69f30535bc48a8d01c70df6f9546fd0d277b
Nabil Kashyap <nkashya1@swarthmore.edu>
2016-10-17 18:34:21 -0400

    begin adding ajax calls to witness select, set current apparatus

--- components/js/WitnessSelect.js
+++ components/js/WitnessSelect.js
@@ -1,7 +1,29 @@
 var React = require('react');
+var _ = require('lodash');
 
 var WitnessSelect = React.createClass({
+
+    handleSelect: function(e){
+        e.preventDefault();
+        return this.props.onSelect(e.target.value);
+    },
+
+    handleAdd: function(){
+        return this.props.onAdd();
+    },
+
+    handleDelete: function(){
+        return this.props.onDelete();
+    },
+
     render: function(){
+
+        var workspaceOptions = _.map(this.props.workspaces, function(item,index) {
+
+            return(
+                <option value = { item.workspaceName } key = { index }>{ item.workspaceName }</option>
+            );
+        });
 
         return (
 
@@ -10,15 +32,16 @@ var WitnessSelect = React.createClass({
             <div className="form-inline pull-left col-md-4 col-md-offset-1">
             <div className="form-group">
             <label htmlFor="baseSelect"></label>
-            <select id="baseSelect" className="form-control">
-            <option value="akh">Akhmatova</option>
+            <select id="baseSelect" className="form-control" onChange={ this.handleSelect }>
+            <option></option>
+            { workspaceOptions }
             </select>
             </div>        
             </div>
 
             <div className="pull-right col-md-4">
-            <button className="btn btn-primary btn-success"><span className="glyphicon glyphicon-plus"></span></button>
-            <button className="btn btn-primary btn-danger"><span className="glyphicon glyphicon-remove"></span></button>
+            <button className="btn btn-primary btn-success"><span className="glyphicon glyphicon-plus" onClick = { this.handleAdd }></span></button>
+            <button className="btn btn-primary btn-danger"><span className="glyphicon glyphicon-remove" onClick = { this.handleDelete }></span></button>
             </div>
 
             </div>
--- components/js/app.js
+++ components/js/app.js
@@ -12,29 +12,123 @@ var MainInterface = React.createClass({
     getInitialState: function() {
         return {
 
+            workspaces: [],
             currentBase: "",
-            currentWorkspace: "",
-            currentWorkspaceID: "",
 
-            // { "witID": "author"}
+            // { "workspaceName":"","sheetID":""}
+            currentWorkspace: {},
+
+            // [{ "witID": "author"}]
             witnesses: [],
 
-            // "appID": [ "witID", "isVisible", "reading" ]
-            apparatus: {}
+            // {"appID": 
+                    // [ int witID, str reading ]
+            apparatus: {},
+
+            googleAPI: {
+                scope: 'https://sheets.googleapis.com/v4/spreadsheets',
+                key: `${googleAPIKey}`            }
+            }
+    }, // getInitialState
+
+    componentDidMount: function(){
+        this.serverRequest = $.get('./js/data.json',function(d){
+
+            var data = d;
+            this.setState({
+                workspaces: data
+            });
+        }.bind(this));
+
+    }, // componentDidMount
+
+    componentWillUnmount: function(){
+        this.serverRequest.abort();
+    }, // componentWillUnmount
+
+    setCurrentWorkspace: function(workspaceName){
+
+        if( this.state.currentWorkspace == ""){
+            $('#baseSelect option:first-child').remove();
         }
+
+        var currentWorkspace = _.find(this.state.workspaces,function(o){
+            return o.workspaceName == workspaceName;
+        });
+
+        var currentApparatus = this.setCurrentApparatus(currentWorkspace.sheetID);
+
+        this.setState({ 
+            currentWorkspace: currentWorkspace,
+            apparatus: currentApparatus
+        });
+    }, // setCurrentWorkspace
+
+    setCurrentApparatus: function(sheetID){
+
+        var currentApparatus = {};
+        $.ajax({
+
+            url: [this.state.googleAPI.scope,
+                  sheetID,
+                  'values',
+                  'Sheet1'].join('/'),
+            data: {
+                    key: this.state.googleAPI.key,
+                    majorDimension: 'COLUMNS'
+                },
+            success: function(d){
+                _.each(d.values[0], function(v,i){
+
+                    // TODO for each app
+
+                    // currReading = []
+                    // currReading[0] = witID
+                    // currReading[1] = value
+
+                    // currApp = {}
+                    // currApp["'" + i + "'"][0] = []
+                    // currApp["'" + i + "'"][0] = currReading
+
+                });
+            }
+        });
     },
+
+    addWorkspace: function(){
+
+        // TODO post data to json php
+        this.setState({});
+
+    }, // addWorkspace
+
+    deleteWorkspace: function(){
+
+        var currentWorkspaces = _.filter(this.state.workspaces,function(o){
+            return o.workspaceName !== this.state.currentWorkspace.workspaceName;
+        }.bind(this));
+
+        // TODO post data to json php
+        this.setState({ workspaces: currentWorkspaces});
+
+    }, // deleteWorkspace
 
     render: function(){
         return (
             <div id="interface">
-            <WitnessSelect />
+            <WitnessSelect 
+                workspaces = { this.state.workspaces }
+                onSelect = { this.setCurrentWorkspace }
+                onAdd = { this.addWorkspace }
+                onDelete = { this.deleteWorkspace }
+            />
             <div className="row">
             <Witness />
             <ReadingsCompare />
             </div>
             </div>
-        );
-    }
+        ); // return
+    } // render
 });
 
 ReactDOM.render(<MainInterface/>, document.getElementById('main'));
